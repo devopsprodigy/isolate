08babfd9161acdd259c64ee10cfd69ee8512a427
Ilya Yakovlev <ilya.yakovlev@me.com>
2017-07-05 18:52:47 +0300

    add asn lookup

--- shared/helper.py
+++ shared/helper.py
@@ -14,6 +14,7 @@ import re
 from redis import Redis
 from operator import itemgetter
 from pyzabbix import ZabbixAPI
+import GeoIP
 # from IsolateCore import __version__
 
 
@@ -322,7 +323,10 @@ class ServerConnection(object):
 
 class AuthHelper(object):
 
+    ASN_DB = '/opt/auth/shared/geoip/GeoIPASNum.dat'
+
     def __init__(self, args, unknown_args):
+        self.geoip = GeoIP.open(self.ASN_DB, GeoIP.GEOIP_STANDARD)
         self.uuid = str(uuid4())
         self.time_start = time()
         self.args = args
@@ -563,6 +567,7 @@ class AuthHelper(object):
                 match_info.append('exact: {}'.format(host['exact_match']))
 
             host['match_info'] = self.colorize(', '.join(match_info), color='okgreen')
+            host['asn'] = self.geoip.name_by_addr(host['server_ip'])
 
         return host
 
